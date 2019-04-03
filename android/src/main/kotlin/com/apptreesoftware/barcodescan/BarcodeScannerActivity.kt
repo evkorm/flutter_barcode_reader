1daf6a38728c3e5c1e33fce3c6a487cf6d31661d
Evgeny Kormilitsin <evgeny.kormilitsin@gmail.com>
2019-04-04 01:28:15 +0700

    Localized button text.

--- android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
@@ -20,7 +20,7 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
     companion object {
         val REQUEST_TAKE_PHOTO_CAMERA_PERMISSION = 100
         val TOGGLE_FLASH = 200
-
+        val CLOSE_ACTIVITY = 300
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,15 +34,19 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        if (scannerView.flash) {
-            val item = menu.add(0,
-                    TOGGLE_FLASH, 0, "Flash Off")
-            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-        } else {
-            val item = menu.add(0,
-                    TOGGLE_FLASH, 0, "Flash On")
-            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-        }
+        val item = menu.add(0,
+                CLOSE_ACTIVITY, 0, "Закрыть")
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+
+//        if (scannerView.flash) {
+//            val item = menu.add(0,
+//                    TOGGLE_FLASH, 0, "")
+//            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+//        } else {
+//            val item = menu.add(0,
+//                    TOGGLE_FLASH, 0, "Flash On")
+//            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+//        }
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -50,6 +54,10 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
         if (item.itemId == TOGGLE_FLASH) {
             scannerView.flash = !scannerView.flash
             this.invalidateOptionsMenu()
+            return true
+        }
+        if (item.itemId == CLOSE_ACTIVITY) {
+            this.finishWithError("close pressed")
             return true
         }
         return super.onOptionsItemSelected(item)
--- ios/Classes/BarcodeScannerViewController.m
+++ ios/Classes/BarcodeScannerViewController.m
@@ -17,33 +17,39 @@
     self.previewView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:_previewView];
     [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"V:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
+                               constraintsWithVisualFormat:@"V:[previewView]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"previewView": _previewView}]];
     [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"H:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
-  self.scanRect = [[ScannerOverlay alloc] initWithFrame:self.view.bounds];
-  self.scanRect.translatesAutoresizingMaskIntoConstraints = NO;
-  self.scanRect.backgroundColor = UIColor.clearColor;
-  [self.view addSubview:_scanRect];
-  [self.view addConstraints:[NSLayoutConstraint
-                             constraintsWithVisualFormat:@"V:[scanRect]"
-                             options:NSLayoutFormatAlignAllBottom
-                             metrics:nil
-                             views:@{@"scanRect": _scanRect}]];
-  [self.view addConstraints:[NSLayoutConstraint
-                             constraintsWithVisualFormat:@"H:[scanRect]"
-                             options:NSLayoutFormatAlignAllBottom
-                             metrics:nil
-                             views:@{@"scanRect": _scanRect}]];
-  [_scanRect startAnimating];
+                               constraintsWithVisualFormat:@"H:[previewView]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"previewView": _previewView}]];
+    self.scanRect = [[ScannerOverlay alloc] initWithFrame:self.view.bounds];
+    self.scanRect.translatesAutoresizingMaskIntoConstraints = NO;
+    self.scanRect.backgroundColor = UIColor.clearColor;
+    [self.view addSubview:_scanRect];
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:[scanRect]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"scanRect": _scanRect}]];
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"H:[scanRect]"
+                               options:NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"scanRect": _scanRect}]];
+    [_scanRect startAnimating];
+    
     self.scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:_previewView];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
-  [self updateFlashButton];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"close"]
+                                                                             style:UIBarButtonItemStylePlain
+                                                                            target:self
+                                                                            action:@selector(cancel)];
+    
+    [self updateFlashButton];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -55,8 +61,8 @@
         if (success) {
             [self startScan];
         } else {
-          [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"PERMISSION_NOT_GRANTED"];
-          [self dismissViewControllerAnimated:NO completion:nil];
+            [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"PERMISSION_NOT_GRANTED"];
+            [self dismissViewControllerAnimated:NO completion:nil];
         }
     }];
 }
@@ -73,7 +79,7 @@
     NSError *error;
     [self.scanner startScanningWithResultBlock:^(NSArray<AVMetadataMachineReadableCodeObject *> *codes) {
         [self.scanner stopScanning];
-         AVMetadataMachineReadableCodeObject *code = codes.firstObject;
+        AVMetadataMachineReadableCodeObject *code = codes.firstObject;
         if (code) {
             [self.delegate barcodeScannerViewController:self didScanBarcodeWithResult:code.stringValue];
             [self dismissViewControllerAnimated:NO completion:nil];
@@ -90,13 +96,15 @@
         return;
     }
     if (self.isFlashOn) {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash Off"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"flash-off"]
                                                                                   style:UIBarButtonItemStylePlain
-                                                                                 target:self action:@selector(toggle)];
+                                                                                 target:self
+                                                                                 action:@selector(toggle)];
     } else {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash On"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"flash-on"]
                                                                                   style:UIBarButtonItemStylePlain
-                                                                                 target:self action:@selector(toggle)];
+                                                                                 target:self
+                                                                                 action:@selector(toggle)];
     }
 }
 
@@ -124,7 +132,7 @@
 - (void)toggleFlash:(BOOL)on {
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (!device) return;
-
+    
     NSError *err;
     if (device.hasFlash && device.hasTorch) {
         [device lockForConfiguration:&err];
